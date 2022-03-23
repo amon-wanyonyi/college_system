c77e9bc08842c35c1f74e01a6791f18854adb990
amon <amon@amon.amon>
2022-03-22 18:43:38 -1000

    Define models

--- college/models.py
+++ college/models.py
@@ -1,3 +1,15 @@
+from msilib.schema import SelfReg
+from typing_extensions import Self
+from unicodedata import name
 from django.db import models
+from pendulum import duration
+from tables import Description
 
 # Create your models here.
+class Courses(models.Model):
+    name=models.CharField(max_length=200,null=True)
+    duration=models.IntegerField(default=3,null=True,blank=True)
+    fees=models.IntegerField(null=True,blank=True)
+    Description=models.CharField(null=True,blank=True,max_length=10000)
+    def _str_(Self):
+        return str(SelfReg.name)
